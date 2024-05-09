bbfc09a8ec34180ca6f332c7a38a2508c6318909
PieterAndriesBerg <p-a.berg@outlook.com>
2024-05-09 11:48:38 +0200

    raylib .gitignore was causing the issues

---
+++ external/raylib/out/build/x64-Debug/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/raylib/src
+# Build directory: G:/raylib/out/build/x64-Debug/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
